f65ba4d3453591b94ddd501618a9fd58dd3a31ac
arthurmdros <arthurmedeiros2005@gmail.com>
2022-08-12 15:48:36 -0300

    Adicionando navegação para o componente de novo produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,16 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
 
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { ForDirective } from './components/directives/for.directive';
+import { RedDirective } from './components/directives/red.directive';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
+import { ProductCreateComponent } from './components/template/product/product-create/product-create.component';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { RedDirective } from './components/directives/red.directive';
-import { ForDirective } from './components/directives/for.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +27,8 @@ import { ForDirective } from './components/directives/for.directive';
     HomeComponent,
     ProductCrudComponent,
     RedDirective,
-    ForDirective
+    ForDirective,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +37,8 @@ import { ForDirective } from './components/directives/for.directive';
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
